a9d81c0941d2af092a8441d60a1519a04648253c
Solarin <solarinjohnson@gmail.com>
2023-12-15 18:14:46 +0100

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import './App.scss';
 import Collection from './components/collection/collection';
 import Display from './components/display/display';
-import './layout/layout.scss'
 import Navigation from './components/navigation/navigation';
 import Cart from './components/cart';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
+  const links = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navigation />
+          <Display />
+          <Collection />
+          <Cart />
+        </>
+      ),
+    }])
   return <div className='container'>
-    <Navigation />
-    <Display />
-    <Collection />
-    <Cart />
+    <RouterProvider router={links} />
   </div>
 }
 
