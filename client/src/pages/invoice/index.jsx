12541f9a68d5a97b8509659cdb55d67ca9f53381
Solarin <solarinjohnson@gmail.com>
2023-12-25 00:26:01 +0100

    christmas 😑

--- client/src/pages/invoice/index.jsx
+++ client/src/pages/invoice/index.jsx
@@ -5,6 +5,12 @@ export default function Invoice() {
       <div className="invoice-head"></div>
       <h1 className="invoice-title">Purchase Order</h1>
       <div className="invoice-line" />
+      <div className="invoice-table"></div>
+      <div className="invoice-line" />
+      <div className="invoice-total">
+        <span>Sum Total</span>
+        <span>{`NGN ${(2000).toLocaleString()}`}</span>
+      </div>
     </div>
   );
 }
