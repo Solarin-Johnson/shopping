f6c3026d12bdfa8b00414639f23354e624f7b776
Solarin <solarinjohnson@gmail.com>
2023-12-25 01:13:14 +0100

    ... Grrrrrra Bow ...

--- client/src/pages/invoice/index.jsx
+++ client/src/pages/invoice/index.jsx
@@ -5,12 +5,43 @@ export default function Invoice() {
       <div className="invoice-head"></div>
       <h1 className="invoice-title">Purchase Order</h1>
       <div className="invoice-line" />
-      <div className="invoice-table"></div>
+      <div className="invoice-table">
+        <div className="invoice-table-head">
+          <div>Name</div>
+          <div>Price (NGN)</div>
+          <div>Qty</div>
+          <div>Total (NGN)</div>
+        </div>
+        <div className="invoice-line invoice-table-line" />
+        <div className="invoice-items">
+          <InvoiceItems />
+          <InvoiceItems />
+          <InvoiceItems />
+          <InvoiceItems />
+          <InvoiceItems />
+        </div>
+      </div>
       <div className="invoice-line" />
       <div className="invoice-total">
         <span>Sum Total</span>
-        <span>{`NGN ${(2000).toLocaleString()}`}</span>
+        <span>{(2000).toFixed(2).toLocaleString()}</span>
       </div>
     </div>
+  );
+}
+
+export function InvoiceItems({ name = "Product Name", price = 800, qty = 2 }) {
+  return (
+    <>
+      <div className="invoice-items-container">
+        <div className="invoice-items-name">Product Name</div>
+        <div className="invoice-items-price">
+          {price.toFixed(2).toLocaleString()}
+        </div>
+        <div className="invoice-items-qty">{qty}</div>
+        <div className="invoice-items-total">{price * qty}</div>
+      </div>
+      <div className="invoice-line invoice-table-line invoice-items-line" />
+    </>
   );
 }
